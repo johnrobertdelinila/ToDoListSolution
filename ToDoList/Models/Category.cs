92b6ccaa5f548e61e401b02ac145b4b01bf757be
johnrobertdelinila <johnrobert.delinila@lorma.edu>
2024-06-03 08:07:03 +0800

    first commit

---
+++ ToDoList/Models/Category.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Category
+  {
+    public int CategoryId { get; set; }
+
+    public string Name { get; set; }
+
+    public List<Item> Items { get; set; }
+  }
+}
